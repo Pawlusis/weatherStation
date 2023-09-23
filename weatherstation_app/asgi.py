bdfce702b9abf58ff70c018d437eabaf6a19895e
Paweł Korzeniewski <pawel.korzeniewski@atos.net>
2023-09-23 21:27:58 +0200

    test commit with app name change

--- weatherstation_app/asgi.py
+++ weatherstation_app/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for weatherstation project.
+ASGI config for weatherstation_app project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weatherstation.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weatherstation_app.settings')
 
 application = get_asgi_application()
--- weatherstation_app/urls.py
+++ weatherstation_app/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for weatherstation project.
+URL configuration for weatherstation_app project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
