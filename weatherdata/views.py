559430f8203caf21e10b10a0285391184360f014
PawelRaspberry <pawlusis12@gmail.com>
2023-09-25 13:21:43 +0200

    updates directly on raspberry

--- weatherdata/views.py
+++ weatherdata/views.py
@@ -4,5 +4,8 @@ from .models import WeatherData
 
 def show_data(request):
     weather_data = WeatherData.objects.all()
-    context = {'weather_data': weather_data}
+    context = {
+		'weather_data': weather_data,
+		'current_data': list(weather_data)[-1] if weather_data.exists() else None,
+		}
     return render(request, 'weatherdata/data.html', context)
